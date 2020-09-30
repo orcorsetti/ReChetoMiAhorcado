53ea406591dc77e4bb406ddaed5c694f8f083991
Ornela Cosetti <orcorsetti@gmail.com>
2020-09-29 22:04:43 -0300

    Cla

--- Business Entities/Ahorcado.cs
+++ Business Entities/Ahorcado.cs
@@ -8,12 +8,53 @@ namespace Business_Entities
 {
     public class Ahorcado
     {
-        //List<Char> LetrasCorrectas = new List<char>();
-        //List<Char> LetrasIncorrectas = new List<char>();
 
-        public bool ControlaLetraActual(char LetraActual)
+        public Ahorcado()
         {
-            return char.IsLetter(char.ToLower(LetraActual));
+            _cantIntentos = 7;
+        }
+
+        private List<Char> letrasCorrectas;
+
+        private List<Char> letrasIncorrectas;
+
+        private char _letra;
+
+        private int _cantIntentos;
+
+        public char Letra { get => _letra; set => _letra = char.ToLower(value); }
+        public int CantIntentos { get => _cantIntentos; set => _cantIntentos = value; }
+        public List<char> LetrasCorrectas { get => letrasCorrectas; set => letrasCorrectas = value; }
+        public List<char> LetrasIncorrectas { get => letrasIncorrectas; set => letrasIncorrectas = value; }
+
+        Palabra pal = new Palabra();
+        
+        public bool ControlaLetraActual()
+        {
+            return char.IsLetter(char.ToLower(Letra));
+        }
+
+        public int RetornaCantLetras()
+        {
+            return pal.PalRandom.Length;
+        }
+
+        public bool PreguntaLetra()
+        {
+            return pal.PalRandom.Contains(Letra);
+        }
+
+        public void JuegaLetra()
+        {
+            if (PreguntaLetra())
+            {
+                LetrasCorrectas.Add(Letra);
+            }
+            else
+            {
+                LetrasIncorrectas.Add(Letra);
+                CantIntentos -= 1;
+            }
         }
     }
 }
--- ReChetoMiAhoracado/TestsJuego.cs
+++ ReChetoMiAhoracado/TestsJuego.cs
@@ -9,12 +9,94 @@ namespace ReChetoMiAhoracado
     [TestClass]
     public class TestsJuego
     {
-        Ahorcado juego = new Ahorcado();
-        
+        Ahorcado Juego = new Ahorcado();
+        public void IniciaJuego(char c)
+        {
+            Juego.Letra = c;
+        }
+
         [TestMethod]
         public void ControlaLetraIngresada()
         {
-            Assert.AreEqual(juego.ControlaLetraActual('C'), true);
+            IniciaJuego('c');
+            Assert.AreEqual(Juego.ControlaLetraActual(), true);
         }
+
+        [TestMethod]
+
+        public void ControlaCantLetrasPalabra()
+        {
+            IniciaJuego('c');
+            Assert.AreEqual(Juego.RetornaCantLetras(), 6);
+        }
+
+        [TestMethod]
+
+        public void VerificarLetraEnPalabra()
+        {
+            IniciaJuego('a');
+            Assert.AreEqual(Juego.PreguntaLetra() , true);
+        }
+
+        [TestMethod]
+
+        public void ControlaCantIntentos()
+        {
+            List<char> ListDeLetras = new List<char>();
+            ListDeLetras.Add('a');
+            ListDeLetras.Add('s');
+            ListDeLetras.Add('t');
+            ListDeLetras.Add('y');
+            ListDeLetras.Add('l');
+            foreach(var i in ListDeLetras)
+            {
+                IniciaJuego(i);
+                Juego.JuegaLetra();
+            }
+
+            Assert.AreEqual(Juego.CantIntentos, 5);
+        }
+
+        [TestMethod]
+
+        public void ControlaListaCorrecta()
+        {
+            List<char> ListDeLetras = new List<char>();
+            ListDeLetras.Add('a');
+            ListDeLetras.Add('s');
+            ListDeLetras.Add('t');
+            ListDeLetras.Add('y');
+            ListDeLetras.Add('l');
+            foreach (var i in ListDeLetras)
+            {
+                IniciaJuego(i);
+                Juego.JuegaLetra();
+            }
+            List<char> letrasCorrectas = new List<char>();
+            letrasCorrectas.Add('a');
+            letrasCorrectas.Add('s');
+            letrasCorrectas.Add('l');
+
+            Assert.Equals(Juego.LetrasCorrectas.Equals(letrasCorrectas), true );
+        }
+
+        /*[TestMethod]
+
+        public void ControlaListaIncorrecta()
+        {
+            List<char> ListDeLetras = new List<char>();
+            ListDeLetras.Add('a');
+            ListDeLetras.Add('s');
+            ListDeLetras.Add('t');
+            ListDeLetras.Add('y');
+            ListDeLetras.Add('l');
+            foreach (var i in ListDeLetras)
+            {
+                IniciaJuego(i);
+                Juego.JuegaLetra();
+            }
+            Assert.AreEqual(Juego.CantIntentos, 5);
+        }*/
+
     }
 }
